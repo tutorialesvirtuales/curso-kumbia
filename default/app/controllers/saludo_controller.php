bac4f4bfcec4a54dfd3b1b52136189624366e75e
tutorialesvirtuales <luzarami@gmail.com>
2013-11-07 00:21:21 -0500

    Primera Parte del Template
    Este commit corresponde al video 3 de Kumbia PHP

--- default/app/controllers/saludo_controller.php
+++ default/app/controllers/saludo_controller.php
@@ -3,9 +3,11 @@
 class SaludoController extends AppController{
     
     public function hola($nombre, $apellido){
+        //View::template('default');
+         $this->titulo = 'Probando Vista';
          $this->nombre = $nombre;
          $this->apellido = $apellido;
-    }
+    } 
     
     public function bye(){
         
